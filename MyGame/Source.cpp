8ae44f4a7d9ff79003a50b0d9306cb8a0a444523
Vladimir Klepach <v8@email.ua>
2020-11-24 13:53:13 +0000

    Added constructor and destructor

--- MyGame/Source.cpp
+++ MyGame/Source.cpp
@@ -2,6 +2,39 @@
 #include <windows.h>
 using namespace std;
 
+const int X = 10;
+const int Y = 8;
+
+struct Player {
+	char show;
+	short team;
+
+};
+Player *** field;
+
+
+void constructor() {
+	field = new Player**[X];
+	for (size_t i = 0; i < X; i++)
+	{
+		field[i] = new Player*[Y];
+
+		//memset(field[i], 0, sizeof(Player) * Y);
+		for (size_t j = 0; j < Y; j++)
+		{
+			field[i][j] = NULL;
+		}
+	}
+}
+
+void destructor() {
+	for (int i = 0; i < X; i++)
+	{
+		delete []field[i];
+	}
+	delete field;
+}
+
 void main() {
 	cout << "Hello World"<<endl;
 
